3a7a598fc529b38f673ef80ba9a7c7d11a46079c
catharina-know <catharina.frindt@knowit.se>
2024-06-28 15:13:00 +0200

    change film function to use data load tool

--- swapi/assets.py
+++ swapi/assets.py
@@ -1,7 +1,7 @@
 
-import requests
+import dlt
+from dlt.sources.helpers import requests
 from dagster import asset, graph
-from dagster_duckdb import DuckDBResource
 import pandas as pd
 
 # TODO: Verifiera att vi har data i snowflake
@@ -10,12 +10,12 @@ import pandas as pd
 
 
 
-@asset(
-    key_prefix=["raw_swapi"]
-)
-def films() -> pd.DataFrame:
+@asset()
+def films() -> None:
+    data = []
     response = requests.get('https://swapi.dev/api/films/')
-    return pd.DataFrame(response.json()["results"])
+    data.append(response.json()["results"])
+    create_and_run_pipeline(data)
 
 @asset(
     key_prefix=["raw_swapi"]
@@ -30,6 +30,16 @@ def vehicles() -> pd.DataFrame:
 def people() -> pd.DataFrame:
     response = requests.get('https://swapi.dev/api/people/')
     return pd.DataFrame(response.json()["results"])
+
+def create_and_run_pipeline(data) -> None:
+
+    pipeline = dlt.pipeline(
+        pipeline_name="film",
+        destination="snowflake",
+        dataset_name="film_data3"
+    )
+
+    pipeline.run(data, table_name="film")
 
 @graph
 def star_wars_graph():
