4e3718cfd12c19f4964aa81739f800317e201af9
catharina-know <catharina.frindt@knowit.se>
2024-06-28 16:18:11 +0200

    load all data using data load tool

--- swapi/assets.py
+++ swapi/assets.py
@@ -2,7 +2,6 @@
 import dlt
 from dlt.sources.helpers import requests
 from dagster import asset, graph
-import pandas as pd
 
 # TODO: Verifiera att vi har data i snowflake
 # X TODO: Ta bort staging assets
@@ -13,33 +12,36 @@ import pandas as pd
 @asset()
 def films() -> None:
     data = []
-    response = requests.get('https://swapi.dev/api/films/')
+    url = 'https://swapi.dev/api/films/'
+    response = requests.get(url)
     data.append(response.json()["results"])
-    create_and_run_pipeline(data)
+    create_and_run_pipeline(data, url.split('/')[4].strip())
 
-@asset(
-    key_prefix=["raw_swapi"]
-)
-def vehicles() -> pd.DataFrame:
-    response = requests.get('https://swapi.dev/api/vehicles/')
-    return pd.DataFrame(response.json()["results"])
+@asset()
+def vehicles() -> None:
+    data = []
+    url = 'https://swapi.dev/api/vehicles/'
+    response = requests.get(url)
+    data.append(response.json()["results"])
+    create_and_run_pipeline(data, url.split('/')[4].strip())
 
-@asset(
-    key_prefix=["raw_swapi"]
-)
-def people() -> pd.DataFrame:
-    response = requests.get('https://swapi.dev/api/people/')
-    return pd.DataFrame(response.json()["results"])
+@asset()
+def people() -> None:
+    data = []
+    url = 'https://swapi.dev/api/people/'
+    response = requests.get(url)
+    data.append(response.json()["results"])
+    create_and_run_pipeline(data, url.split('/')[4].strip())
 
-def create_and_run_pipeline(data) -> None:
+def create_and_run_pipeline(data, data_subject: str) -> None:
 
     pipeline = dlt.pipeline(
-        pipeline_name="film",
+        pipeline_name=f"{data_subject}_pipeline",
         destination="snowflake",
-        dataset_name="film_data3"
+        dataset_name=f"{data_subject}_data"
     )
 
-    pipeline.run(data, table_name="film")
+    pipeline.run(data, table_name=f"{data_subject}")
 
 @graph
 def star_wars_graph():
